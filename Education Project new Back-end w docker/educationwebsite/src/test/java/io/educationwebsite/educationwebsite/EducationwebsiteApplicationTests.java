9315cb73648db79c7bdbadaf406416bc9d652614
Mariam Saad <103897877+Mariam-Saad512003@users.noreply.github.com>
2024-05-08 13:38:56 +0300

    Add files via upload

---
+++ Education Project new Back-end w docker/educationwebsite/src/test/java/io/educationwebsite/educationwebsite/EducationwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.educationwebsite.educationwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
