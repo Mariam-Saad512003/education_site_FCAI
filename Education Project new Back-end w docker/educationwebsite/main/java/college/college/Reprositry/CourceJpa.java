911273a39211cf733ebfa98a497e3d8ae725b3fc
Mariam Saad <103897877+Mariam-Saad512003@users.noreply.github.com>
2024-05-08 13:45:00 +0300

    Delete Education Project new Back-end w docker/educationwebsite/main directory

--- Education Project new Back-end w docker/educationwebsite/main/java/college/college/Reprositry/CourceJpa.java
+++
@@ -1,8 +0,0 @@
-package college.college.Reprositry;
-
-import college.college.Model.CourceClass;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourceJpa extends JpaRepository<CourceClass,Integer> {
-}
